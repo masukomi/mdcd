951dadf7ec10cd5a291c4b95a6037b05a950abde
masukomi <masukomi@masukomi.org>
2015-11-22 09:05:03 -0500

    [scheme] improved unit tests

--- chicken-scheme/mdcd-test.scm
+++ chicken-scheme/mdcd-test.scm
@@ -5,11 +5,8 @@
 (import mdcd)
 (use files)
 (use directory-utils)
-
-;(use filepath)
-  ; ^^^ would use it
-  ; to test that it's cleaning up names correctly
-  ; but it sucks and doesn't compile... i think.
+(use srfi-13)
+(use filepath)
 
 (define (remove-file file-path)
   (if (file-exists? file-path)
@@ -34,14 +31,40 @@ Hah!")
 )
   (test-group "MDCD"
     (test-group "file paths"
-      (test-group "dir differences"
-        (test-assert (not (equal? docfun-file-path docfun-var-path)))
-        (test-assert (not (equal? docfun-file-path docfun-syntax-path)))
-        (test-assert (not (equal? docfun-var-path docfun-syntax-path)))
+      (test-group "directories"
+        ; they functions, variables, and syntax things should 
+        ; all be stored in different places
+        (test-assert "funtions not stored in same dir as variables"
+                     (not (equal? docfun-fun-path docfun-var-path)))
+        (test-assert "functions not stored in same dir as syntax"
+                     (not (equal? docfun-fun-path docfun-syntax-path)))
+        (test-assert "variables not stored in same dir as syntax"
+                     (not (equal? docfun-var-path docfun-syntax-path)))
+        (let ((manual-fun-path (string-concatenate (list
+                         (get-mdcd-home) ; ends with a /
+                         "functions/test-mdcd-docfun.md")))
+              (manual-var-path (string-concatenate (list
+                         (get-mdcd-home) ; ends with a /
+                         "variables/test-mdcd-docfun.md")))
+              (manual-syntax-path (string-concatenate (list
+                         (get-mdcd-home) ; ends with a /
+                         "syntax/test-mdcd-docfun.md"))))
+        (test "functions stored in correct dir"
+              manual-fun-path docfun-fun-path)
+        (test "variable stored in correct dir" 
+              manual-var-path docfun-var-path)
+        (test "syntax stored in correct dir"
+              manual-syntax-path docfun-syntax-path)
+
+        )
       )
       (test-group "naming"
-        (test "horribly_named_fun_with_crap_" 
-              (mdcd-path-for-fun "horribly/named:fun|with*crap?" ))
+        ; doesn't do space removal because you can't name
+        ; anything with a space in scheme
+        (test "function names converted to safe file names" 
+              "horribly_named_fun_with_crap_" 
+              (filepath:take-base-name
+                (mdcd-path-for-fun "horribly/named:fun|with*crap?" )))
       )
     )
     ; (test-group "file names"
@@ -52,15 +75,27 @@ Hah!")
         ; so, first delete it.
         (remove-file docfun-fun-path)
         (doc-fun "test-mdcd-docfun" test-doc-string)
-        (test-assert (file-exists? docfun-fun-path))
+        (test-assert "documenting a function creates a file"
+                     (file-exists? docfun-fun-path))
+        
         (remove-file docfun-fun-path) ; cleanup
       )
-      ; (test-group "variables"
-      ;
-      ; )
-      ; (test-group "syntax"
-      ;
-      ; )
+      (test-group "variables"
+        (remove-file docfun-var-path)
+        (doc-var "test-mdcd-docfun" test-doc-string)
+        (test-assert "documenting a variable creates a file"
+                     (file-exists? docfun-var-path))
+        (remove-file docfun-var-path) ; cleanup
+
+      )
+      (test-group "syntax"
+        (remove-file docfun-syntax-path)
+        (doc-syntax "test-mdcd-docfun" test-doc-string)
+        (test-assert "documenting syntax creates a file"
+                     (file-exists? docfun-syntax-path))
+        (remove-file docfun-syntax-path) ; cleanup
+     
+      )
     )
   )
 )
